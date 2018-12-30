ddf875b7e5995ff66c11cbf3bae5b17038a6300c
Manfred Diaz <takeitallsource@gmail.com>
2018-12-29 19:02:09 -0500

    [FIX] Undeclared variables being used.

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
 		if done: 
 
 			if total_timesteps != 0: 
-				print("Total T: %d Episode Num: %d Episode T: %d Reward: %f") % (total_timesteps, episode_num, episode_timesteps, episode_reward)
+				print("Total T: %d Episode Num: %d Episode T: %d Reward: %f" % (total_timesteps, episode_num, episode_timesteps, episode_reward))
 				if args.policy_name == "TD3":
 					policy.train(replay_buffer, episode_timesteps, args.batch_size, args.discount, args.tau, args.policy_noise, args.noise_clip, args.policy_freq)
 				else: 
